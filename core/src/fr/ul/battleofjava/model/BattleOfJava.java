30bdf2555eed3937769daee93ec68663cca6d6d8
bertra162u <matt.bert@live.fr>
2019-11-21 15:49:44 +0100

    some things

--- core/src/fr/ul/battleofjava/model/BattleOfJava.java
+++ core/src/fr/ul/battleofjava/model/BattleOfJava.java
@@ -1,4 +1,17 @@
 package fr.ul.battleofjava.model;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import fr.ul.battleofjava.model.board.Board;
+
 public class BattleOfJava {
+
+    private Board player1Board;
+    private Board player2Board;
+
+    public void render (SpriteBatch sb) {
+
+
+
+    }
+
 }
