0d0e0cc0319c77bccfa0af8e01bfd5a33c192a5b
licho2u <dylan.li-cho1@etu.univ-lorraine.fr>
2019-11-21 15:01:16 +0100

    Initial commit

---
+++ desktop/src/fr/ul/battleofjava/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.ul.battleofjava.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.ul.battleofjava.BattleOfJava;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleOfJava(), config);
+	}
+}
