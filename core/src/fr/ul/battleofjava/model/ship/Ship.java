bf0ae0e04dcae024d2662e6872b4895e16388ade
licho2u <dylan.li-cho1@etu.univ-lorraine.fr>
2019-11-21 16:33:03 +0100

    Création du bateau, il faut encore gérer l'orientation

--- core/src/fr/ul/battleofjava/model/ship/Ship.java
+++ core/src/fr/ul/battleofjava/model/ship/Ship.java
@@ -1,4 +1,55 @@
 package fr.ul.battleofjava.model.ship;
 
 public abstract class Ship {
+
+    private int hp;
+    private int x;
+    private int y;
+    private int width;
+    private int heigth;
+    private int orientation;
+
+    /**
+     * Constructor of the Ship
+     * @param hp Health point of the ship
+     * @param x  Absciss
+     * @param y Ordinate
+     * @param width Width of the ship
+     * @param heigth Height of the ship
+     * @param orientation Orientation of the ship
+     */
+    public Ship(int hp, int x, int y, int width, int heigth, int orientation){
+        this.hp = hp;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.heigth = heigth;
+        this.orientation = orientation;
+    }
+
+    /**
+     * Return true if the HP of the ship is under zero, False else
+     * @return true if the HP of the ship is under zero, False else
+     */
+    public boolean isSunk(){
+        return hp<=0;
+    }
+
+    /**
+     * The ship take damage
+     */
+    public void damage(){
+        hp = hp - 1;
+    }
+
+    /**
+     * Set the position of the ship
+     * @param x absissa
+     * @param y ordinate
+     */
+    public void setPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
 }
