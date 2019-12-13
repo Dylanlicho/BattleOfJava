c6b9ffaebdc5356277ba3a6e5d1a8009f4c3a511
licho2u <dylan.li-cho1@etu.univ-lorraine.fr>
2019-12-13 16:55:45 +0100

    Ajout des bateaux du 20éme siècle Ajout d'un champ num dans Player pour savoir si c'est le J1 ou J2 Instanciation des bateaux

--- src/sample/model/factory/AgeFactory.java
+++ src/sample/model/factory/AgeFactory.java
@@ -2,6 +2,14 @@ package sample.model.factory;
 
 
 import sample.model.ship.Ship;
+import sample.model.ship.century15s.Caravelle;
+import sample.model.ship.century15s.Drakkar;
+import sample.model.ship.century15s.Flette;
+import sample.model.ship.century15s.Sailboat;
+import sample.model.ship.century20s.Corvette;
+import sample.model.ship.century20s.Cruiser;
+import sample.model.ship.century20s.Destroyer;
+import sample.model.ship.century20s.Ironclad;
 import sample.model.ship.century15s.*;
 
 import java.util.ArrayList;
@@ -13,9 +21,11 @@ public class AgeFactory {
      * @param age Century
      * @return A list of 7 ships of century age
      */
-    public ArrayList<Ship> createNewShip(int age){
+    public static ArrayList<Ship> createNewShip(int age){
         if (age == GameFactory.CENTURY15S) {
             return create15SShip();
+        }else if (age == GameFactory.CENTURY20S){
+            return create20SShip();
         }
         return new ArrayList<Ship>();
     }
@@ -24,7 +34,7 @@ public class AgeFactory {
      * Create 7 ships of the century 15
      * @return A list of 7 ships of century 15
      */
-    public ArrayList<Ship> create15SShip(){
+    private static ArrayList<Ship> create15SShip(){
         ArrayList<Ship> ships = new ArrayList<Ship>();
 
         Sailboat sailboat = new Sailboat(1,0,0,GameFactory.LEFT);
@@ -42,6 +52,31 @@ public class AgeFactory {
         ships.add(flette2);
         ships.add(flette3);
         ships.add(drakkar);
+        return ships;
+    }
+
+    /**
+     * Create 7 ships of the century 20
+     * @return A list of 7 ships of century 20
+     */
+    private static ArrayList<Ship> create20SShip(){
+        ArrayList<Ship> ships = new ArrayList<Ship>();
+
+        Corvette corvette = new Corvette(1,0,0,GameFactory.LEFT);
+        Destroyer destroyer1 = new Destroyer(1,0,corvette.getY()+Century20SFactory.WIDTHCORVETTE + 1,GameFactory.LEFT);
+        Destroyer destroyer2 = new Destroyer(1,0,destroyer1.getY()+Century20SFactory.WIDTHDESTROYER+ 1,GameFactory.LEFT);
+        Cruiser cruiser1 = new Cruiser(1,0,destroyer2.getY()+Century20SFactory.WIDTHDESTROYER + 1,GameFactory.LEFT);
+        Cruiser cruiser2 = new Cruiser(1,0,cruiser1.getY()+Century20SFactory.WIDTHCRUISER + 1,GameFactory.LEFT);
+        Cruiser cruiser3 = new Cruiser(1,0,cruiser2.getY()+Century20SFactory.WIDTHCRUISER + 1,GameFactory.LEFT);
+        Ironclad ironclad = new Ironclad(1,0,cruiser3.getY()+Century20SFactory.WIDTHCRUISER + 1,GameFactory.LEFT);
+
+        ships.add(corvette);
+        ships.add(destroyer1);
+        ships.add(destroyer2);
+        ships.add(cruiser1);
+        ships.add(cruiser2);
+        ships.add(cruiser3);
+        ships.add(ironclad);
         return ships;
     }
 
