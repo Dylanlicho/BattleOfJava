4b3c8615ced2d369e05b3ff1cc8a836ae3c99d13
Bert54 <matt.bert@live.fr>
2019-12-21 12:13:28 +0100

    even more tests

--- src/main/tests/BattleOfJavaTests.java
+++ src/main/tests/BattleOfJavaTests.java
@@ -17,7 +17,64 @@ public class BattleOfJavaTests {
         assertDoesNotThrow(() -> {
             boj.setShipPosition(p, s, -10, -10);
         });
-        
+
+    }
+
+    @Test
+    void setShipPositionTooBigCoordinates() {
+
+        BattleOfJava boj = new BattleOfJava();
+        Player p = EasyMock.createMock(Player.class);
+        Ship s = EasyMock.createMock(Ship.class);
+        assertDoesNotThrow(() -> {
+            boj.setShipPosition(p, s, 10000000, 10000000);
+        });
+    }
+
+    @Test
+    void startNewGameBadCentury() {
+
+        BattleOfJava boj = new BattleOfJava();
+        assertDoesNotThrow(() -> {
+            boj.startNewGame(-10, 0);
+        });
+
+    }
+
+    @Test
+    void startNewGameBadTactics() {
+
+        BattleOfJava boj = new BattleOfJava();
+        assertDoesNotThrow(() -> {
+            boj.startNewGame(15, -10);
+        });
+
+    }
+
+    @Test
+    void shootNegativeCoordinates() {
+        BattleOfJava boj = new BattleOfJava();
+        Player p = EasyMock.createMock(Player.class);
+        assertDoesNotThrow(() -> {
+            boj.shoot(p,-10, -10);
+        });
+    }
+
+    @Test
+    void shootTooBigCoordinates() {
+        BattleOfJava boj = new BattleOfJava();
+        Player p = EasyMock.createMock(Player.class);
+        assertDoesNotThrow(() -> {
+            boj.shoot(p,10000000, 10000000);
+        });
+    }
+
+    @Test
+    void shootNullPlayer() {
+        BattleOfJava boj = new BattleOfJava();
+        assertDoesNotThrow(() -> {
+            boj.shoot(null,0, 0);
+        });
     }
 
 }
